4dfba212fca46e59419722bd62426b5894b66536
sunoda <59427857+sunoda@users.noreply.github.com>
2021-06-09 11:15:24 +0800

    function done
    functional button

--- e-commerce/src/Contain.js
+++ e-commerce/src/Contain.js
@@ -1,49 +1,130 @@
-function contain() {
+import axios from 'axios';
+import { useCallback, useEffect, useState } from 'react';
+import { ReactComponent as Payment} from './img/payment.svg';
+import { ReactComponent as Diploma} from './img/diploma.svg';
+
+function Contain() {
+  const [ forename, setForename ] = useState('');
+  const [ surname, setSurname ] = useState('');
+  const [ infoTitle, setInfoTitle ] = useState('');
+  const [ infoText, setInfoText ] = useState('');
+  const [ successMsg, setSuccessMsg ] = useState();
+
+  const [ firstStep, setFirstStep ] = useState(true);
+  const [ secondStep, setSecondStep ] = useState(false);
+  const [ finalStep, setFinalStep ] = useState(false);
+
+  const forenameHandler = (e) => {
+    setForename(e.target.value)
+  }
+  const surnameHandler = (e) => {
+    setSurname(e.target.value)
+  }
+
+  const initial = useCallback(() => {
+    const initSet = () => {
+      setInfoTitle('Mis Datos');
+      setInfoText('Revisa los datos y completa aquellas pendientes antes de continuar con el procesa.')
+      setFirstStep(true);
+      setSecondStep(false)
+      setFinalStep(false)
+    }
+    initSet();
+  }, [])
+  useEffect(() => {
+      initial();
+  }, [initial])
+
+  const goFirst = () => {
+    initial()
+    setFirstStep(true)
+    setSecondStep(false)
+  }
+  const goSecond = () => {
+    setFirstStep(false)
+    setSecondStep(true)
+    setInfoTitle('Pago del product')
+    setInfoText('Una vez efectuado el pago del producto, recibiras un email con los de detolles de la comprp.')
+    axios.post('http://www.mocky.io/v2/5e3d41272d00003f7ed95c09', {forename, surname}).then(async (res) => {
+      await setSuccessMsg(res.data)
+    })
+  }
+  const goFinish = () => {
+    setFirstStep(false)
+    setSecondStep(false)
+    setFinalStep(true)
+    setInfoTitle(successMsg.title)
+    setInfoText(successMsg.text)
+  }
+
   return (
     <div className="contains">
       <div className="step_flow">
         <div className="step1">
-          <div className="step active">
-            <span>1</span>
-            {/* <i className="fas fa-check"></i> */}
+          <div className={`step ${firstStep ? 'active ' : secondStep ? 'done ' : 'done'}`}>
+            <span className={secondStep ? 'hide' : finalStep ? 'hide' : ''}>1</span>
+            <i className={`fas fa-check ${firstStep ? 'hide' : secondStep ? '' : finalStep ? '' : 'hide'}`}></i>
           </div>
           <span>MIS DATOS</span>
         </div>
         <div className="step_line"></div>
         <div className="step2">
-          <div className="step">
-            <span>2</span>
-            {/* <i className="fas fa-check"></i> */}
+          <div className={`step ${firstStep ? '' : secondStep ? 'active ' : finalStep ? 'done' : ''}`}>
+            <span className={finalStep ? 'hide' : ''}>2</span>
+            <i className={`fas fa-check ${firstStep ? 'hide' : secondStep ? 'hide' : ''}`}></i>
           </div>
           <span>PAGO</span>
         </div>
       </div>
       <div className="info">
         <div className="describe">
-          <h1>Mis Datos</h1>
-          <span>
-            Revisa los datos y completa aquellas pendientes antes de continuar con el procesa.
-          </span>
+          <Diploma className={finalStep ? '' : 'hide'}></Diploma>
+          <h1>{infoTitle}</h1>
+          <span>{infoText}</span>
         </div>
         <div className="detail">
-          <div className="forename">
-            <span>Nombre</span>
-            <input type="text" />
+          <div className={`input_name ${firstStep ? '' : 'hide'}`}>
+            <div className="forename">
+              <span>Nombre</span>
+              <input type="text" value={forename} onChange={forenameHandler}/>
+            </div>
+            <div className="surname">
+              <span>Apelidos</span>
+              <input type="text" value={surname} onChange={surnameHandler}/>
+            </div>
           </div>
-          <div className="surname">
-            <span>Apelidos</span>
-            <input type="text" />
+          <div className={`credit_check ${secondStep ? '' : 'hide'}`}>
+            <Payment className="payment"></Payment>
+            <h1 className="payment_title">
+                Tarjeta de credito
+            </h1>
+            <div className="payment_text">
+              Se redirigira a una platforma de pago, Se trata de un proceso seguro. Puede que la validacion del pago tarde 24 horas en completarse.
+            </div>
+            <div className="icons">
+              <i className="fab fa-cc-visa"></i>
+              <i className="fab fa-cc-mastercard"></i>
+            </div>
           </div>
         </div>
-        <div className="action">
-          <div className="btn-next">
-            <span>Continuar</span>
+        <div className={`action ${firstStep ? 'first_step' : ''} ${secondStep ? 'second_step' : ''}`}>
+          <div className={'btn_next first_step ' + (firstStep ? '' : 'hide')} onClick={goSecond}>
+              <span>Continuar</span>
+              <i className="fas fa-arrow-circle-right"></i>
+          </div>
+          <div className={'btn_prev second_step ' + (secondStep ? '' : 'hide')} onClick={goFirst}>
+            <i className="fas fa-arrow-circle-left"></i>
+            <span>Anterior</span>
+          </div>
+          <div className={'btn_next second_step ' + (secondStep ? '' : 'hide')} onClick={goFinish}>
+            <span>Pagar</span>
             <i className="fas fa-arrow-circle-right"></i>
           </div>
         </div>
       </div>
+      
     </div>
   )
 }
 
-export default contain
+export default Contain
